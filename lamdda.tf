36aa826d63eb869a427242bfaf5716c6c634ecff
Ravinder Sirohi <ravinder@hovergen.com>
2026-06-04 13:45:02 +0100

    Initial setup.

---
+++ lamdda.tf
@@ -0,0 +1,47 @@
+# IAM role for Lambda execution
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Package the Lambda function code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/index.js"
+  output_path = "${path.module}/lambda/function.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "lambda_function" {
+  filename      = data.archive_file.lambda_zip.output_path
+  function_name = "demo_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.handler"
+  code_sha256   = data.archive_file.lambda_zip.output_base64sha256
+  runtime       = "nodejs24.x"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "example"
+  }
+}
